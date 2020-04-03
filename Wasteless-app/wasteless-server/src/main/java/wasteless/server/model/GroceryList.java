5fe5bd3faa8c56f6df143678cf74a78705cbbc10
cezarasabou <s_cezara@yahoo.com>
2020-04-03 20:10:39 +0300

    Added features

---
+++ Wasteless-app/wasteless-server/src/main/java/wasteless/server/model/GroceryList.java
@@ -0,0 +1,63 @@
+package wasteless.server.model;
+
+import wasteless.server.model.Item;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "grocery_list")
+public class GroceryList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "list_name", nullable = false)
+    private String groceryListName;
+    @OneToMany(mappedBy = "groceryList", cascade = CascadeType.ALL)
+    private List<Item> groceryItems;
+
+    @ManyToOne
+    @JoinColumn(name="userId", nullable = false)
+    private User user;
+
+    public GroceryList(String groceryListName) {
+        this.groceryListName = groceryListName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Item> getGroceryItems() {
+        return groceryItems;
+    }
+
+    public void setGroceryItems(List<Item> groceryItems) {
+        this.groceryItems = groceryItems;
+    }
+
+    public String getGroceryListName() {
+        return groceryListName;
+    }
+
+    public void setGroceryListName(String groceryListName) {
+        this.groceryListName = groceryListName;
+    }
+
+    public void addGroceryItem(Item newItem){
+        groceryItems.add(newItem);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ Wasteless-app/wasteless-server/src/main/java/wasteless/server/model/User.java
@@ -0,0 +1,68 @@
+package wasteless.server.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email_address", nullable = false)
+    private String emailAddress;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<GroceryList> groceryLists;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<GroceryList> getGroceryLists() {
+        return groceryLists;
+    }
+
+    public void setGroceryLists(List<GroceryList> groceryLists) {
+        this.groceryLists = groceryLists;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailAddress
+                + "]";
+    }
+
+}
+
